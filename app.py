965540d9481e1f4307215acebc3d366531efaeb5
vishwasswami2-debug <vishwasswami2@gmail.com>
2026-09-11 23:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import datetime, timezone
@@ -55,7 +56,6 @@ def market_data():
     }
 
     return jsonify(data)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
