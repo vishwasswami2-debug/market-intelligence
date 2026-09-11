b8fc5ad4d12a8a4fa751d73a44b124d6774fcb3e
vishwasswami2-debug <vishwasswami2@gmail.com>
2026-09-11 19:32:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Market Intelligence Backend is running"
+
+
+@app.route("/api/health")
+def health():
+    return jsonify({
+        "status": "ok",
+        "message": "Market Intelligence backend is running"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
